9d36ad96b3a7572871260c8bd031b436fbe90789
Kaleemullah Ahsan <159533186+kaleemullahahsan@users.noreply.github.com>
2024-09-04 19:13:27 +0500

    Delete Piroll directory

--- Piroll/jquery.js
+++
@@ -1,8 +0,0 @@
-  $(document).ready(function(){
-    $('#menu-toggle').click(function(){
-        $('#mobile-menu').toggleClass('show');
-    })
-    $('#menuClose').click(function(){
-        $('#mobile-menu').removeClass('show')
-    });
-  });
